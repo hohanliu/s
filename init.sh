8556131593ce6fcd88f495a9ea8842ea5a04f737
John Liu <108072260+hohanliu@users.noreply.github.com>
2022-08-28 17:29:21 +0800

    added bash

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
 sudo apt-get -y install ibus-chewing gsettings-desktop-schemas
+sleep 3
 gsettings set org.freedesktop.ibus.general preload-engines "['xkb:us::eng', 'chewing']"
 ibus-daemon &
