803c230d3bec46c2d18d91df17a8f2cbc2a8a134
hohanliu <108072260+hohanliu@users.noreply.github.com>
2022-08-30 10:15:25 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 sudo apt-get -y install ibus-chewing gsettings-desktop-schemas
-sleep 3
 gsettings set org.freedesktop.ibus.general preload-engines "['xkb:us::eng', 'chewing']"
-ibus-daemon &
+ibus-daemon
