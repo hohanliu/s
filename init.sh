b50c883fad58d1511f1b52f1e0c467e51315eac0
hohanliu <108072260+hohanliu@users.noreply.github.com>
2022-08-30 14:29:25 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,10 +7,10 @@ ibus-daemon
 # install extensions via terminal
 # fill the array with the needed extensions 
 # key=["extension_name"] value="extension_ID"
-declare -A EXTlist=(
-    ["line"]="ophjlpahpchlmihnnnihgmmeilfjmjjc"
-)
-for i in "${!EXTlist[@]}"; do
-    # echo "Key: $i value: ${EXTlist[$i]}"
-    echo '{"external_update_url": "https://clients2.google.com/service/update2/crx"}' > /home/kasm-user/.config/google-chrome/Default/Extensions/${EXTlist[$i]}.json
-done
+# declare -A EXTlist=(
+#     ["line"]="ophjlpahpchlmihnnnihgmmeilfjmjjc"
+# )
+# for i in "${!EXTlist[@]}"; do
+#     # echo "Key: $i value: ${EXTlist[$i]}"
+#     echo '{"external_update_url": "https://clients2.google.com/service/update2/crx"}' > /opt/google/chrome/extensions/${EXTlist[$i]}.json
+# done
